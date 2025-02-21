591b495c8687d72729d1636805d9d62d24dd5c82
Soulless5141 <138739109+Soulless5141@users.noreply.github.com>
2025-02-21 14:36:24 +0900

    Vector2D 実装

--- GameJam_2025_TeamO/GameJam_2025_TeamO/Objects/RemainingBullets/RemainingBullets.cpp
+++ GameJam_2025_TeamO/GameJam_2025_TeamO/Objects/RemainingBullets/RemainingBullets.cpp
@@ -1,4 +1,5 @@
 #include "RemainingBullets.h"
+#include "../../Utility/ResourceManager.h"
 
 RemainingBullets::RemainingBullets() : life_bullet(0)
 {
@@ -10,7 +11,11 @@ RemainingBullets::~RemainingBullets()
 
 void RemainingBullets::Initialize()
 {
+	ResourceManager* rm = ResourceManager::GetInstance();
 	life_bullet = 5;
+
+	image = rm->GetImages("Resource/Images/bullet.png", 1, 1, 1, 32, 32);
+	
 }
 
 void RemainingBullets::Update()
