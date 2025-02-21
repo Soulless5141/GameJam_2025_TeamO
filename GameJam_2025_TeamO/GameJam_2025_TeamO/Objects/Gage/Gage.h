c7754a0fe7960a6f13136bc4a748891711a9d51c
Soulless5141 <138739109+Soulless5141@users.noreply.github.com>
2025-02-21 11:40:12 +0900

    ゲージ作りかけ

--- GameJam_2025_TeamO/GameJam_2025_TeamO/Objects/Gage/Gage.h
+++ GameJam_2025_TeamO/GameJam_2025_TeamO/Objects/Gage/Gage.h
@@ -1,10 +1,13 @@
 #pragma once
+#include "../../Utility/PadInput.h"
+
 class Gage
 {
 private:
 	int meter;				// ゲージの中身
 	int fluctuation;        // ゲージの変動値
 	bool change_flip_flag;  // ゲージが増えてるか減っているのか
+	bool decision;			// Aボタンが押されたか
 
 public:
 	Gage();
