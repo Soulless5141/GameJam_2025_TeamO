d618bf31f190db43cca20ce35c7ab0428337d23b
nagaminesho <sho-nagamine-k22h@stu.kbc.ac.jp>
2025-02-22 09:14:14 +0900

    selecttargetエラー修正

--- GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/GameScene/SelectTarget.cpp
+++ GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/GameScene/SelectTarget.cpp
@@ -18,14 +18,16 @@ void SelectSceneInit()
 {
 }
 
-//eSceneType SelectTarget::SelectSceneUpdate()
-//{
-//	//選択した的からシーン遷移
-//	if (ForcusFlg == TRUE)
-//	{
-//
-//	}
-//}
+eSceneType SelectTarget::SelectSceneUpdate()
+{
+	//選択した的からシーン遷移
+	if (ForcusFlg == TRUE)
+	{
+
+	}
+
+	return eSelectMode;
+}
 
 void SelectTarget::SelectSceneDraw(void)
 {
