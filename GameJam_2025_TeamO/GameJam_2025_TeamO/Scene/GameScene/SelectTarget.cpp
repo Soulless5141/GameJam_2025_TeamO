c367663cc8f72bcd677c5bd60da4eb667438757c
nagaminesho <sho-nagamine-k22h@stu.kbc.ac.jp>
2025-02-24 10:24:25 +0900

    仮画像

--- GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/GameScene/SelectTarget.cpp
+++ GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/GameScene/SelectTarget.cpp
@@ -36,6 +36,15 @@ void SelectTarget::SelectSceneDraw(void)
 	player->Draw();
 	DrawFormatString(0, 0, 0xffffff, "select scene");
 	DrawGraph(0, 0, background, TRUE);
+
+	// “I‚Ì‰¼•`‰æ
+	DrawBox(100, 100, 200, 200, 0xffffff, TRUE);
+	DrawBox(250, 100, 350, 200, 0xffffff, TRUE);
+	DrawBox(400, 100, 500, 200, 0xffffff, TRUE);
+
+	DrawBox(100, 250, 200, 350, 0xffffff, TRUE);
+	DrawBox(250, 250, 350, 350, 0xffffff, TRUE);
+	DrawBox(400, 250, 500, 350, 0xffffff, TRUE);
 }
 
 eSceneType SelectTarget::GetNowScene() const
--- GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/SceneManager.cpp
+++ GameJam_2025_TeamO/GameJam_2025_TeamO/Scene/SceneManager.cpp
@@ -26,7 +26,7 @@ void SceneInit(eSceneType new_scene_type);
 void SceneManagerInitialize(void)
 {
 	is_end_flag = FALSE;
-	ChangeScene(eTitle);
+	ChangeScene(eSelectMode);
 }
 
 //シーンのアップデート
